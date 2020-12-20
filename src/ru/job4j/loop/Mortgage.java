277ac43713f9ca334f9ee489f6a0f2cf38603862
dkozhuhov <dkozhuhov@samokat.ru>
2020-12-20 14:56:29 +0700

    5.7. Ипотека [#434259]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+
+    public static int year(int amount, int salary, double percent) {
+        int year = 0;
+        while (amount >= 0) {
+
+            amount = (int) (amount + amount * (percent / 100));
+            amount = amount - salary;
+            year++;
+
+        }
+        return year;
+    }
+}
